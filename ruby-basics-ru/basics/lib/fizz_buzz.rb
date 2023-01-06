60002194eb27f77abfa5db18ca9ba37211e3b8f1
@hexlet/cli <support@hexlet.io>
2023-01-06 21:11:32 +0400

    submit ruby-basics-ru/basics

--- ruby-basics-ru/basics/lib/fizz_buzz.rb
+++ ruby-basics-ru/basics/lib/fizz_buzz.rb
@@ -1,19 +1,24 @@
 # frozen_string_literal: true
 
 # BEGIN
-def fizz_buzz(a, b)
-    result = ''
-    a.upto(b) do |num|
-     if (num.to_f % 3).zero? && (num.to_f % 5).zero?
-      result += 'FizzBuzz '
-     elsif (num.to_f % 3).zero?
-      result += 'Fizz '
-     elsif (num.to_f % 5).zero?
-      result += 'Buzz '
-     else
-      result += num.to_s + " "
-     end
+def fizz_buzz(a = 0, b = 0)
+    if a > b
+        result = ''
+        a.upto(b) do |num|
+        if (num.to_f % 3).zero? && (num.to_f % 5).zero?
+        result += 'FizzBuzz '
+        elsif (num.to_f % 3).zero?
+        result += 'Fizz '
+        elsif (num.to_f % 5).zero?
+        result += 'Buzz '
+        else
+        result += num.to_s + " "
+        end
+        end
+        result[0..result.size-1]
+    else
+        nil
     end
-    result[0..result.size-1]
 end
+puts fizz_buzz.nil?
 # END
